721a581f0e160216f25d15092ffc30ed8c8ad6ae
jieseob1 <jieseob1@gmail.com>
2023-05-21 20:05:01 +0900

    Testing openAI MR Review Function

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router(); // router instance
+
+app.use(ctx => {
+  ctx.body = 'hi koa';
+})
+
+router.get('/', (ctx, next) => {
+  ctx.body = 'home';
+})
+
+router.get('/about', (ctx, next) => {
+  ctx.body = '소개';
+});
+
+router.get('/about/:name', (ctx, next) => {
+  const { name } = ctx.params; // 라우트 경로에서 :파라미터명 으로 정의된 값이 ctx.params 안에 설정됩니다.
+  ctx.body = name + '의 소개';
+});
+
+router.get('/post', (ctx, next) => {
+  const { id } = ctx.request.query; // 주소 뒤에 ?id=10 이런식으로 작성된 쿼리는 ctx.request.query 에 파싱됩니다.
+  if(id) {
+      ctx.body = '포스트 #' + id;
+  } else {
+      ctx.body = '포스트 아이디가 없습니다.';
+  }
+});
+
+// app.use(router.routes());
+// app.use(router.allowedMethods());
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(4000, () => {
+  console.log("listen this");
+})
