571805378d928b3d2a6749b68fac5d53b9cb9ed7
jiseob park <jieseob1@gmail.com>
2023-09-06 17:45:44 +0900

    Implement Basic CRUD Function of Organization

--- src/main/java/enhanced_inventory/server/domain/Organization.java
+++ src/main/java/enhanced_inventory/server/domain/Organization.java
@@ -1,4 +1,16 @@
 package enhanced_inventory.server.domain;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Organization {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private String id;
+
+  private String name;
+  private String type;
+  private String companyId;
+  // 필요한 다른 필드들
 }
